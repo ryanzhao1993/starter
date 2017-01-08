7a09bbafedf5bd040ecb48f3f21a33f114f4dc72
ryanzhao1993 <ryanzhao1993@qq.com>
2017-01-08 19:56:01 +0800

    add middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import counter from './reducers';
 
-let store = createStore(counter);
+import createLogger from 'redux-logger';
+
+// let store = createStore(counter);
+
+//添加middleware
+// 第一种写法
+// let store = createStore(counter, applyMiddleware(createLogger()));
+
+// 第二种写法
+const finalCreateStore = applyMiddleware(createLogger())(createStore);
+let store = finalCreateStore(counter);
 export default store;
